5cf883eb8c5c9c1048363d85fa2cc2b26e8dfe55
mofe <mofeomotosho8@gmail.com>
2022-03-11 00:56:06 -0300

    adding nav items

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,15 @@
 import './Body.css';
+import { FaImages } from "react-icons/fa";
 
 function Body (){
     return(
         <div className='Body'>
             <nav className='body-Nav'>
-                mofee
+                <button className='body-Nav-Btn'>
+                    <FaImages className='boby-Nav-icon'/>
+                    <p>Images</p>
+                </button>
+                
             </nav>
             <body className='body-Body'>
                 bodyy
