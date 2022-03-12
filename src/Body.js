b16402ca5c19ea1ec9dc72585a1afc3ba6197a73
mofe <mofeomotosho8@gmail.com>
2022-03-11 22:56:51 -0300

    adding extended div

--- src/Body.js
+++ src/Body.js
@@ -3,32 +3,69 @@ import { FaImages } from "react-icons/fa";
 import { GoTextSize } from "react-icons/go";
 import { BsThreeDots } from "react-icons/bs";
 
+
+function showExtendNav(){   
+    document.getElementById('navExtendiv').style.display = "flex" 
+}
+
+function hideExtendNav(){   
+    document.getElementById('navExtendiv').style.display = "none" 
+}
+
+function NavExtend (){
+    return(
+        <div id='navExtendiv' className='nav-Extend'>
+            <button onClick={hideExtendNav}>back</button>
+        </div>
+    )
+
+};
+
+function Nav(){
+    return(
+        <nav className='body-Nav'>
+            <button className='body-Nav-Btn'>
+                <FaImages className='boby-Nav-icon'/>
+                <p>Images</p>
+            </button>
+
+            <button id='textBtn' className='body-Nav-Btn' onClick={showExtendNav}>
+                <GoTextSize className='boby-Nav-icon'/>
+                <p>Text</p>
+            </button>
+
+            <button className='body-Nav-Btn'>
+                <BsThreeDots className='boby-Nav-icon'/>
+                <p>More</p>
+            </button>
+        </nav>
+    )
+};
+
+
+
+function Main(){
+    return(
+        <div className='body-Body'>
+                <div className='edit-Box-Div'>
+                    <div className='text-Div'>
+                        <input className='text-Box'></input>
+                    </div>
+                    
+
+                </div>               
+            </div>
+    )
+};
+
+
 function Body (){
     
     return(
         <div className='Body'>
-            <nav className='body-Nav'>
-                <button className='body-Nav-Btn'>
-                    <FaImages className='boby-Nav-icon'/>
-                    <p>Images</p>
-                </button>
-
-                <button className='body-Nav-Btn'>
-                    <GoTextSize className='boby-Nav-icon'/>
-                    <p>Text</p>
-                </button>
-
-                <button className='body-Nav-Btn'>
-                    <BsThreeDots className='boby-Nav-icon'/>
-                    <p>More</p>
-                </button>
-                
-            </nav>
-            <div className='body-Body'>
-                <div className='edit-Box-Div'>
-
-                </div>               
-            </div>
+           <Nav/>
+           <NavExtend/>
+           <Main/>
         </div>
     )
 };
