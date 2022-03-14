2b0bcecfe767034beb23063026bbfce50d11e959
mofe <mofeomotosho8@gmail.com>
2022-03-13 23:55:14 -0300

    adding state to extended nav

--- src/Body.js
+++ src/Body.js
@@ -3,6 +3,7 @@ import { FaImages } from "react-icons/fa";
 import { GoTextSize } from "react-icons/go";
 import { BsThreeDots } from "react-icons/bs";
 import { MdOutlineArrowBackIosNew, MdClose } from "react-icons/md";
+import React from 'react';
 
 
 function showExtendNav(){   
@@ -15,6 +16,12 @@ function hideExtendNav(){
     document.getElementById("editBoxid").classList.replace( 'edit-Box-Div-small','edit-Box-Div');
     
 }
+
+
+
+
+
+
 function NavExtendImg(){
     return(
         <div id='navExtendiv' className='nav-Extend'>
@@ -27,46 +34,58 @@ function NavExtendImg(){
         </div>
     )
 };
-function NavExtend (){
-    return(
-        <div id='navExtendiv' className='nav-Extend'>
-            <div className='nav-Extend-header'>
-                <p>Text</p>
-                <button className='nav-Extend-backBtn' onClick={hideExtendNav}><MdClose className='nav-Extend-backIcon-x'/></button>
-                <button className='nav-Extend-backBtn' onClick={hideExtendNav}><MdOutlineArrowBackIosNew className='nav-Extend-backIcon'/></button>
 
-            </div>
-            <div className='nav-Extend-Body'>
-                    
-                <input className='text-Box' placeholder='Top Text'></input>
-                <input className='text-Box' placeholder='Bottom Text'></input>
-                <button className='add-Text-Btn'>Add Text</button>
-                   
-            </div>
-
+const TextExtended = (
+    <>
+        <div className='nav-Extend-header'>
+            <p>Text</p>
+            <button className='nav-Extend-backBtn' onClick={hideExtendNav}><MdClose className='nav-Extend-backIcon-x'/></button>
+            <button className='nav-Extend-backBtn' onClick={hideExtendNav}><MdOutlineArrowBackIosNew className='nav-Extend-backIcon'/></button>
         </div>
-    )
+        <div className='nav-Extend-Body'>
+                        
+            <input className='text-Box' placeholder='Top Text'></input>
+            <input className='text-Box' placeholder='Bottom Text'></input>
+            <button className='add-Text-Btn'>Add Text</button>
+                    
+        </div>
+    </> 
+)
 
-};
+
 
 function Nav(){
+    const [extendedNav, setExtendedNav] = React.useState(<></>)
+    
+        
+    function addText(){
+        setExtendedNav(TextExtended)
+        showExtendNav()
+    }
     return(
-        <nav className='body-Nav'>
-            <button className='body-Nav-Btn'>
-                <FaImages className='boby-Nav-icon'/>
-                <p>Images</p>
-            </button>
+        <>
+            <nav className='body-Nav'>
+                <button className='body-Nav-Btn'>
+                    <FaImages className='boby-Nav-icon'/>
+                    <p>Images</p>
+                </button>
 
-            <button id='textBtn' className='body-Nav-Btn' onClick={showExtendNav}>
-                <GoTextSize className='boby-Nav-icon'/>
-                <p>Text</p>
-            </button>
+                <button id='textBtn' className='body-Nav-Btn' onClick={addText}>
+                    <GoTextSize className='boby-Nav-icon'/>
+                    <p>Text</p>
+                </button>
 
-            <button className='body-Nav-Btn'>
-                <BsThreeDots className='boby-Nav-icon'/>
-                <p>More</p>
-            </button>
-        </nav>
+                <button className='body-Nav-Btn'>
+                    <BsThreeDots className='boby-Nav-icon'/>
+                    <p>More</p>
+                </button>
+            </nav>
+
+            <div id='navExtendiv' className='nav-Extend'>
+                {extendedNav}
+            </div>
+        </>
+        
     )
 };
 
@@ -78,17 +97,18 @@ function Main(){
                 <div id='editBoxid' className='edit-Box-Div'>
                     
                 </div>               
-            </div>
+        </div>
+        
     )
 };
 
 
 function Body (){
     
+    
     return(
         <div className='Body'>
            <Nav/>
-           <NavExtend/>
            <Main/>
         </div>
     )
